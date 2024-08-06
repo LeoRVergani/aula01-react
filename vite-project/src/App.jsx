10a1a398648733890ae926fffd1d8ba9ac6a2e83
leorvergani <vergani@outlook.com.br>
2024-08-06 00:27:20 -0300

    curso-react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {Cabecalho} from './components';
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <Cabecalho />
+    </>
+  );
+};
+
+export {App};
